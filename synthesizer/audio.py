90b82baeef3b31d79c36563beb60e66abc075912
Jim Lai <jim-plus@users.noreply.github.com>
2025-03-29 01:59:48 -0400

    Modernizations

--- synthesizer/audio.py
+++ synthesizer/audio.py
@@ -107,7 +107,7 @@ def _griffin_lim(S, hparams):
     Based on https://github.com/librosa/librosa/issues/434
     """
     angles = np.exp(2j * np.pi * np.random.rand(*S.shape))
-    S_complex = np.abs(S).astype(np.complex)
+    S_complex = np.abs(S).astype(np.cdouble)
     y = _istft(S_complex * angles, hparams)
     for i in range(hparams.griffin_lim_iters):
         angles = np.exp(1j * np.angle(_stft(y, hparams)))
@@ -167,7 +167,7 @@ def _mel_to_linear(mel_spectrogram, hparams):
 
 def _build_mel_basis(hparams):
     assert hparams.fmax <= hparams.sample_rate // 2
-    return librosa.filters.mel(hparams.sample_rate, hparams.n_fft, n_mels=hparams.num_mels,
+    return librosa.filters.mel(sr=hparams.sample_rate, n_fft=hparams.n_fft, n_mels=hparams.num_mels,
                                fmin=hparams.fmin, fmax=hparams.fmax)
 
 def _amp_to_db(x, hparams):
